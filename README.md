cb11f2ae6315a359a74e3ba59ef2f31be5511931
Mauricio Barrera <mauricio.barrerag@gmail.com>
2016-06-23 10:51:50 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sometests
+Dummy repo for some code for test
---
+++ ruby/first_test.rb
@@ -0,0 +1,25 @@
+books = {
+	"MX" => "52",
+	"USA" => "01",
+	"CO" => "321"
+   }
+
+#
+def print_cities(books)
+  books.each{|k,v| puts k}
+end
+
+def print_ccode(books, key)
+  puts books[key]
+end
+
+loop do 
+	puts "Give a city and i will display the country code"
+	key = gets.chomp
+	puts "Getting #{key} country"
+	if books.key?(key)
+      print_ccode(books, key)
+    else
+      print_cities(books)
+    end
+end
