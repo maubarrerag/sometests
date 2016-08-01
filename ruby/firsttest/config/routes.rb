dd6d00c9cd65459e4fca1724b80600d7a86991bc
Mauricio Barrera <mauricio.barrerag@gmail.com>
2016-08-01 17:08:29 -0500

    Lesson 100 - Users signup

--- ruby/firsttest/config/routes.rb
+++ ruby/firsttest/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   resources :comments
-  resources :users
+  resources :users, except: [:new]
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   
 end
