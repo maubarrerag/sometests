67d5bb4eb4afc959b9eee85a1c2eda1e2ecca4cb
Mauricio Barrera <mauricio.barrerag@gmail.com>
2016-08-01 16:47:42 -0500

    Lesson 98 - Add Authenticate Password

--- ruby/firsttest/app/models/user.rb
+++ ruby/firsttest/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	before_save: { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i;
+	before_save { self.email = email.downcase }
 	validates :username, presence: true, uniqueness: { case_sensitive: false }, 
-	          lenght: { minimum: 8, maximum: 50 }
+	          length: { minimum: 8, maximum: 50 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }, 
               format: { with: VALID_EMAIL_REGEX }
 
 	has_many :comments
 	has_many :articles
+	has_secure_password
 end
